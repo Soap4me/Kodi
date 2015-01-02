57d96ca36a5a2c6f0af832a82b23b1c075465fae
Ufian <gimlis2talk@gmail.com>
2015-01-02 17:53:21 +0300

    Add login, cookies and token part

--- soap4api/soapapi.py
+++ soap4api/soapapi.py
@@ -6,18 +6,24 @@ import cookielib
 import gzip
 import os, os.path
 import tempfile
+import time
 import urllib
 import urllib2
 import StringIO
 
 try:
-	import json
+    import json
 except:
-	import simplejson as json
+    import simplejson as json
+
+
+class SoapException(Exception):
+    pass
 
 
 class SoapApi(object):
-    def __init__(self, path=None):
+
+    def __init__(self, path=None, auth=None):
         if path is None:
             path = tempfile.gettempdir()
             path = os.path.join(path, 'soap4kobi')
@@ -26,6 +32,7 @@ class SoapApi(object):
         self.path = path
         self.token = None
         self.CJ = cookielib.CookieJar()
+        self.auth = auth
 
     def _cookies_init(self):
         urllib2.install_opener(
@@ -34,7 +41,7 @@ class SoapApi(object):
             )
         )
 
-        self.cookie_path = os.path.join(self.path, 'cookie')
+        self.cookie_path = os.path.join(self.path, 'cookies')
         if not os.path.exists(self.cookie_path):
             os.makedirs(self.cookie_path)
             #print '[%s]: os.makedirs(cookie_path=%s)' % (addon_id, cookie_path)
@@ -98,17 +105,63 @@ class SoapApi(object):
 
         return text
 
-    def login(self, username, password):
+    def _save_token(self, data):
+        token_path = os.path.join(self.path, "token")
+        with open(token_path, "w") as f:
+            f.write(json.dumps(data))
+
+    def _load_token(self, from_login=False):
+        token_path = os.path.join(self.path, "token")
+        if not os.path.exists(token_path):
+            return False
+
+        with open(token_path, "r") as f:
+            dump = f.read()
+            data = json.loads(dump)
+
+        if data.get('token') is None or data.get('till', time.time() + 10) <= time.time():
+            if from_login or self.auth is None:
+                raise SoapException("Bad authorization. Token process.")
+
+            self.login()
+
+
+        self.token = data.get('token')
+
+    def login(self, username=None, password=None):
+        if username is None and password is None and self.auth is not None \
+                and 'username' in self.auth and 'password' in self.auth:
+            username = self.auth['username']
+            password = self.auth['password']
+
+        if username is None or password is None:
+            raise SoapException("Bad authorization. Login process.")
+
+
         text = self._request(
             "http://soap4.me/login/",
             post={"login": username, "password": password}
         )
 
-        print text
+        data = json.loads(text)
+
+        if not isinstance(data, dict) or data.get('ok') != 1:
+            raise SoapException("Bad authorization. Soap4.me process.")
+
+        self._save_token(data)
+        self._load_token(True)
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         print "Use: python soapapi.py <login> <password>"
 
-    s = SoapApi(path=os.path.abspath("."))
-    s.login(sys.argv[1], sys.argv[2])
+    path = os.path.abspath(".")
+    path = os.path.join(path, "soap4_data")
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    s = SoapApi(path, auth={
+        "username": sys.argv[1],
+        "password": sys.argv[2]
+    })
+    s.login()
