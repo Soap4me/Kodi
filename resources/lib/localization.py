8dae7ec775cb06dccee9a4d0437445b16f5b320e
Timur Manyanov <gtibalt@gmail.com>
2017-11-03 11:09:43 +0100

    Add i18n support, en/ru translations

---
+++ resources/lib/localization.py
@@ -0,0 +1,36 @@
+import xbmcaddon
+
+__settings__ = xbmcaddon.Addon(id='plugin.video.soap4.me')
+loc_str = __settings__.getLocalizedString
+
+my_shows = loc_str(32001)
+all_shows = loc_str(32002)
+unfinished = loc_str(32003)
+recommended = loc_str(32004)
+
+last_20 = loc_str(32011)
+with_unwatched = loc_str(32012)
+
+original = loc_str(32021)
+original_sub = loc_str(32022)
+russian_sub = loc_str(32023)
+translated = loc_str(32024)
+
+days_left = loc_str(32031)
+views = loc_str(32032)
+
+add_to_my_shows = loc_str(32041)
+remove_from_my_shows = loc_str(32042)
+mark_as_unwatched = loc_str(32043)
+mark_as_watched = loc_str(32044)
+
+play = loc_str(32101)
+from_time = loc_str(32102)
+from_start = loc_str(32103)
+
+error = loc_str(32201)
+error_msg = loc_str(32202)
+error_auth = loc_str(32203)
+error_cred = loc_str(32204)
+
+done = loc_str(32211)
